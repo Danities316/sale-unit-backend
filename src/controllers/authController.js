a1ac11e30da4ce85bc53ca21362cb74854394571
Danities Ichaba <attay_316@hotmail.com>
2023-09-03 12:53:55 +0100

    authController debugging: final debugging, all route are working perfectly now

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,17 +2,19 @@ const { check, validationResult } = require('express-validator');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const User = require('../../models').User;
-const { forgetPasswordEmail, sendConfirmationEmail } = require('../../config/mailTransport'); // For sending email (you may use a different library)
+const {
+  forgetPasswordEmail,
+  sendConfirmationEmail,
+} = require('../../config/mailTransport'); // For sending email (you may use a different library)
 const dotenv = require('dotenv');
 const crypto = require('crypto');
-const sendVerificationCode = require("../../utils/twilio")
+const sendVerificationCode = require('../../utils/twilio');
 
 dotenv.config();
 
 // Secret key for JWT token
 const JWT_SECRET = process.env.JWT_SECRET;
 // Your AccountSID and Auth Token from console.twilio.com
-
 
 const generateVerificationCode = () => {
   return Math.floor(100000 + Math.random() * 900000).toString();
@@ -41,7 +43,9 @@ exports.registerUser = async (req, res) => {
     // Check if the phone number is already registered
     const existingUser = await User.findOne({ where: { phone } });
     if (existingUser) {
-      return res.status(400).json({ message: 'Phone number already registered' });
+      return res
+        .status(400)
+        .json({ message: 'Phone number already registered' });
     }
 
     // Generate a 6-digit verification code
@@ -50,7 +54,6 @@ exports.registerUser = async (req, res) => {
     try {
       // Send the verification code via SMS using Twilio
       await sendVerificationCode(phone, verificationCode);
-      
 
       // Hash the password before storing it
       const hashedPassword = bcrypt.hashSync(password, 10);
@@ -63,7 +66,7 @@ exports.registerUser = async (req, res) => {
       });
 
       // Generate and send JWT token
-    const token = jwt.sign({ userId: newUser.id }, JWT_SECRET);
+      const token = jwt.sign({ userId: newUser.id }, JWT_SECRET);
       // Respond with a success message
       return res.status(201).json({ token });
     } catch (twilioError) {
@@ -77,25 +80,24 @@ exports.registerUser = async (req, res) => {
 };
 
 exports.updateUserInfo = async (req, res) => {
-  const id =  req.user.userId // comming from the jwt token;
-  console.log("this is the user: ", req.user)
+  const id = req.user.userId; // comming from the jwt token;
+  console.log('this is the user: ', req.user);
   try {
-    const { firstName, lastName, username, businessName, CAC, email } = req.body;
+    const { firstName, lastName, username, businessName, CAC, email } =
+      req.body;
 
     // Validate the incoming data
     if (!firstName || !lastName || !username || !email) {
       return res.status(400).json({ message: 'Missing required fields' });
     }
-   
-    const user =  await User.findAll({ where: { id } });
-  
-    
-    // Check if the user is authenticated 
+
+    const user = await User.findAll({ where: { id } });
+
+    // Check if the user is authenticated
     if (user.isVerified == false) {
       return res.status(401).json({ message: 'user Unauthorized' });
     }
 
-  
     // Update user information in the User table
     const userId = id; // Assuming you have a user ID in your session
     const updatedUser = await User.update(
@@ -107,10 +109,12 @@ exports.updateUserInfo = async (req, res) => {
         CAC,
         email,
       },
-      { where: { id: userId } }
+      { where: { id: userId } },
     );
 
-    return res.status(200).json({ message: 'User information updated successfully' });
+    return res
+      .status(200)
+      .json({ message: 'User information updated successfully' });
   } catch (error) {
     console.error('Error during user information update:', error.message);
     return res.status(500).json({ message: 'Internal Server Error' });
@@ -156,16 +160,26 @@ exports.verifyPhoneNumber = async (req, res) => {
   try {
     const { phone, verificationCode } = req.body;
 
+    // Validate phone number format (You can use a library like 'libphonenumber' for more thorough validation)
+    // if (!/^\d{10}$/.test(phone)) {
+    //   return res.status(400).json({ message: 'Invalid phone number format' });
+    // }
+
     // Find the user by phone number
     const user = await User.findOne({ where: { phone } });
 
     if (!user || user.verificationCode !== verificationCode) {
+      // Implement rate limiting here to prevent abuse
       return res.status(401).json({ message: 'Invalid verification code' });
     }
 
     // Mark the user as verified in the database
     user.isVerified = true;
+    user.verificationCode = null;
     await user.save();
+
+    // Log the successful verification
+    console.log(`Phone number ${phone} verified successfully`);
 
     return res
       .status(200)
@@ -178,31 +192,49 @@ exports.verifyPhoneNumber = async (req, res) => {
 
 exports.forgotPassword = async (req, res) => {
   try {
-    const { email } = req.body;
+    const { phone } = req.body;
 
     // Find the user by email
-    const user = await User.findOne({ where: { email } });
+    const user = await User.findOne({ where: { phone } });
 
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
+    //Change is verified to false
+    user.isVerified = false;
+
+    /**********************This line of code is for sending verification via email */
     // Generate a temporary reset code
-    const resetToken =  crypto.randomBytes(20).toString('hex');
+    // const resetToken = crypto.randomBytes(20).toString('hex');
 
     // Set the reset token and expiration time in the user's record
-    user.resetToken = resetToken;
-    user.resetTokenExpiry = Date.now() + 3600000; // Token expires in 1 hour
+    // user.resetToken = resetToken;
+    // user.resetTokenExpiry = Date.now() + 3600000; // Token expires in 1 hour
+
+    // Send a password reset email to the user
+    // forgetPasswordEmail(req.headers.host, user.name, email, resetToken);
+
+    // Generate a 6-digit verification code
+    const verificationCode = generateVerificationCode();
+
+    // Send the verification code via SMS using Twilio
+    await sendVerificationCode(phone, verificationCode);
+    user.verificationCode = verificationCode;
 
     // Save the user record
     await user.save();
 
-    // Send a password reset email to the user
-    forgetPasswordEmail(req.headers.host, user.name, email, resetToken);
+    //get last 4 digit of users phone numer
+    const getLast4Digits = (phoneNumber) => phoneNumber.slice(-4);
 
-    return res.status(200).json({ message: 'Password reset email sent' });
+    return res.status(200).json({
+      message: `Password reset has been sent to ******${getLast4Digits(
+        phone,
+      )} phone number`,
+    });
   } catch (error) {
-    console.error('Error during password reset request:', error.message);
+    console.error('Error during password reset request:', error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
 };
@@ -242,7 +274,7 @@ exports.resetPassword = async (req, res) => {
     });
   } catch (error) {
     console.error('Error resetting password', error);
-    res.status(500).json({
+    return res.status(500).json({
       error: 'An error occurred while resetting the password!',
     });
   }
--- utils/twilio.js
+++ utils/twilio.js
@@ -1,27 +1,29 @@
 const twilio = require('twilio');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTHTOKEN;  
+const authToken = process.env.TWILIO_AUTHTOKEN;
 
-const  sendVerificationCode = async function (event, context) {
-
+const sendVerificationCode = async function (event, context) {
   try {
-     const message = await  sendMessage(event, context);
-   } catch (error) {
-     console.log('error', error);
-   }
+    return await sendMessage(event, context);
+  } catch (error) {
+    return console.error('Error sending Twilio sms:', error.message);
+  }
+};
 
-  // return context.logStreamName;
-}
-  
-  const sendMessage = async (phone, verificationCode) => {
-    const twilioClient = twilio(accountSid, authToken);
-    return twilioClient.messages.create({
+const sendMessage = async (phone, verificationCode) => {
+  const twilioClient = twilio(accountSid, authToken);
+  return twilioClient.messages
+    .create({
       body: `Your verification code: ${verificationCode}`,
       to: phone,
       from: '+15005550006', // Your Twilio phone number
-    }).then((message) => console.log(message.body)).catch(err => console.log("Delivery error: ", err));;
-    console.log(twilioClient.body)
-  };
+    })
+    .then((message) => console.log(message.body))
+    .catch((err) => {
+      return console.error('Error sending SMS:', err.message);
+    });
+  console.log(twilioClient.body);
+};
 
-  module.exports =  sendVerificationCode
+module.exports = sendVerificationCode;
